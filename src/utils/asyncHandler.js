1d2167a294703c489ac0c088a44269c37d363d93
Shrutii-debug <shrutiagarwal1467@gmail.com>
2025-12-27 21:47:19 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+      }
+}
+
+
+export {asyncHandler}
+
+/*
+const asyncHandler = () => {}
+const asyncHandler = (func) => {() => {}}
+const asyncHandler = (func) => async () =>  {}
+*/
+
+
+// we have just removed the curly brace
+//this is try catch wrapper function, we can also have promises wrapper function
+
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+*/

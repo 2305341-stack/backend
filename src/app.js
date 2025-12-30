a75e8d8abed2cbde8b3a62c38235498f85be4aec
Shrutii-debug <shrutiagarwal1467@gmail.com>
2025-12-30 21:01:17 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -17,4 +17,17 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())  //configuring cookie parser but we dont need options here
 
+
+// routes import
+
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+
+
 export { app }
